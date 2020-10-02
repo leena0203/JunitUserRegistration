86e05aaa76cbcf97e48f8abaea3f1a83733bb0bf
leena0203 <71863423+leena0203@users.noreply.github.com>
2020-10-03 00:58:50 +0530

    Update UserRegJunit.java

--- src/main/java/com/UserRegJunit/UserRegJunit.java
+++ src/main/java/com/UserRegJunit/UserRegJunit.java
@@ -15,13 +15,13 @@ public class UserRegJunit {
 				System.out.println("Mobile Format "+number+" is: "+ userregjunitobj.validateMobileNo(number));
 				String password = sc.nextLine();
 				System.out.println("Password "+password+" is: "+ userregjunitobj.validatePassword(password));
-				String[] multipleEmail;
-				multipleEmail =  new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
+				String[] testEmail;
+				tsetEmail =  new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
 						"abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com",
 						"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*.com", "abc@%*.com",
 						"abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
-				for(int i=0; i< multipleEmail.length; i++) {
-				System.out.println("EmailId "+multipleEmail[i]+" is: "+ userregjunitobj.validateEmail(multipleEmail[i]));
+				for(int i=0; i< testEmail.length; i++) {
+				System.out.println("EmailId "+testEmail[i]+" is: "+ userregjunitobj.validateEmail(testEmail[i]));
 				}
 			}   
 			public String matchpattern(String regex, String input) {   
