046b765d5e987175c054972ad0625c87b33b9122
leena0203 <71863423+leena0203@users.noreply.github.com>
2020-10-03 00:59:42 +0530

    Update UserRegJunit.java

--- src/main/java/com/UserRegJunit/UserRegJunit.java
+++ src/main/java/com/UserRegJunit/UserRegJunit.java
@@ -16,7 +16,7 @@ public class UserRegJunit {
 				String password = sc.nextLine();
 				System.out.println("Password "+password+" is: "+ userregjunitobj.validatePassword(password));
 				String[] testEmail;
-				tsetEmail =  new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
+				testEmail =  new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
 						"abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com",
 						"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*.com", "abc@%*.com",
 						"abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
